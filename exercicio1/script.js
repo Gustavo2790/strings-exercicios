60f3d15e7a87ebc8f43f9509562ef44cf1c98bfe
Gustavo <gustavosoares2790@gmail.com>
2023-08-30 22:22:37 -0300

    Adicionado exercícios de strings

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,9 @@
 function imprimirComidasFavoritas (){
-
+    let nomeDoUsuario = prompt('Digite seu nome:')
+    let comidaFavorita1 = prompt('Digite sua primeira comida favorita:')
+    let comidaFavorita2 = prompt('Digite sua segunda comida favorita:')
+    let comidaFavorita3 = prompt('Digite sua terceira comida favorita:')
+    console.log(`Estas são as comidas favoritas de ${nomeDoUsuario}:\n${comidaFavorita1};\n${comidaFavorita2};\n${comidaFavorita3}.`)
 }
+
+imprimirComidasFavoritas()
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+console.log(frase)
+
+let novaFrase = frase.replace("verde", "rosa")
+console.log(novaFrase)
+let novaFrase1 = novaFrase.replace("azul", "laranja")
+console.log(novaFrase1)
+
